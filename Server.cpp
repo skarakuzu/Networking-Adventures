9a9af17f8f6e3c0a279ce583c8b7c674319ac6aa
skarakuzu <karakuzu.seher@gmail.com>
2024-12-30 15:45:32 -0500

    modified file sending routine to make it send chunks of data in a while loop

--- Server.cpp
+++ Server.cpp
@@ -39,30 +39,53 @@ void Server::handle_connection(int client_socket_fd)
     std::string header{request.responses[HTTP_HEADER]};
     //std::cout<<"cheking address of request class: "<<&request<<std::endl;
     //std::cout<<"cheking address of static vars: "<<&request.responses<<std::endl;
-    std::cout<<"cheking address of mimetype array: "<<&request.mimetype<<std::endl;
+    //std::cout<<"cheking address of mimetype array: "<<&request.mimetype<<std::endl;
+    //std::cout<<"cheking address of messageType enum: "<<&messageType<<std::endl;
 
     if(fdimg < 0 || filePath.length() <= 2)
     {
         header = std::string(request.responses[NOT_FOUND]);
         printf("cannot open file path: %s,\n", filePath.c_str());
+        close(fdimg);
+
+        close(client_socket_fd);
+        return;
     }
     fstat(fdimg, &stat_buf);
     int img_total_size = stat_buf.st_size;
     int block_size = stat_buf.st_blksize;
+    std::cout<<"total byte vs block bytes is: "<<img_total_size<<" "<<block_size<<std::endl;
 
     std::string buffer_write(img_total_size, '\0');
     ssize_t bytes_read;
 
     int read_bytes = read(fdimg, buffer_write.data(), buffer_write.size()-1);
-    std::cout<<"sent bytes is: "<<read_bytes<<std::endl;
+    //std::cout<<"total byte vs sent bytes is: "<<img_total_size<<" "<<read_bytes<<std::endl;
 
     //std::cout<<"header is: "<<header<<std::endl;
 
     if (read_bytes > 0)
     {
-        std::string response = header + request.get_content_type() + buffer_write;
+        //std::string response = header + request.get_content_type() + buffer_write;
+        std::string response = header + request.get_content_type();
         //std::cout<<"Printing the response: "<<response<<std::endl;
         write(client_socket_fd, response.c_str(), response.size());
+
+        int send_bytes = ((img_total_size < block_size) ? img_total_size : block_size );
+        write(client_socket_fd, buffer_write.c_str(), send_bytes);
+        img_total_size = img_total_size - send_bytes;
+
+        size_t sent_size = send_bytes;
+        while(img_total_size > 0)
+        {
+        std::cout<<"here sending data"<<std::endl;
+        int send_bytes = ((img_total_size < block_size) ? img_total_size : block_size );
+        write(client_socket_fd, buffer_write.c_str()+sent_size, send_bytes);
+        img_total_size = img_total_size - send_bytes;
+        sent_size += send_bytes;
+        }
+        std::cout<<"here finished sending data"<<std::endl;
+
     }
 
     close(fdimg);
@@ -81,7 +104,7 @@ void Server::shut_down()
     std::cout<<"!!!!!!!!!!! SHUTTING DOWN THE SERVER !!!!!!!!!!!\n";
     interrupted = 1;
     server_socket->close_socket();
-    std::terminate;
+    throw std::runtime_error("Server is shut down!");
 }
 
 
