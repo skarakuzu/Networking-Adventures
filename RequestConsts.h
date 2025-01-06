c7b617cf983c8c3a76584fc2781eaea40a1ec773
skarakuzu <karakuzu.seher@gmail.com>
2025-01-06 14:23:46 -0500

    added pdf mimetype

--- RequestConsts.h
+++ RequestConsts.h
@@ -19,7 +19,7 @@ enum{
       SERVER_ERROR
  }messageType;
 
-static constexpr std::array<std::pair<std::string_view, std::string_view> , 11> mimetype = 
+static constexpr std::array<std::pair<std::string_view, std::string_view> , 12> mimetype = 
 {
 std::make_pair("json", "Content-Type: application/json\r\n\r\n"),
 std::make_pair("png",  "Content-Type: image/png\r\n\r\n"),
@@ -32,6 +32,7 @@ std::make_pair("css",  "Content-Type: text/css\r\n\r\n"),
 std::make_pair("svg",  "Content-Type: image/svg+xml\r\n\r\n"),
 std::make_pair("ico",  "Content-Type: image/vnd.microsoft.icon\r\n\r\n"),
 std::make_pair("tiff", "Content-Type: image/tiff\r\n\r\n"),
+std::make_pair("pdf",  "Content-Type: application/pdf\r\n\r\n"),
 };
 static constexpr std::array<std::string_view, 5> responses = 
 {
